7d954b6d7793b2c978453157e1bd71b3464e4890
eaulisa <eugenio.aulisa@ttu.edu>
2019-10-07 16:48:06 -0500

    added comments to program

--- Learn 3.py
+++ Learn 3.py
@@ -1,12 +1,12 @@
 # More strings and text
 
-# Program will say, "There are 10 types of peoples."
+# Program defines string variable x as "There are 10 types of peoples."
 x = "There are %d types of peoples." % 10
-# Program defines binary
+# Program defines string binary variable as "binary"
 binary = "binary"
-# Program defines doNot
+# Program defines doNot binary variable as "don't"
 doNot = "don't"
-#Program will say, "Those who know binary and those who don't"
+#Program defines y string variable as "Those who know binary and those who don't"
 y = "Those who know %s and those who %s" % (binary, doNot)
 
 #Program will print the variable x
@@ -14,9 +14,9 @@ print(x)
 #Program will print the variable y
 print(y)
 
-#Program will say, "I said:'There are 10 types of peoples.'.:"
+#Program will print "I said:'There are 10 types of peoples.'.:", note that %r adds quotes around x variable
 print("I said:%r.:" % x)
-#Program will say, "I also said: 'Those who know binary and those who don't'."
+#Program will print, "I also said: 'Those who know binary and those who don't'."
 print("I also said: '%s'." % y)
 
 #Program will define hilarious as "True"
@@ -24,7 +24,7 @@ hilarious = True
 #Program will define jokeEvaluation as "Isn't that joke so funny?! %r"
 jokeEvaluation = "Isn't that joke so funny?! %r"
 
-#Program will say, "Isn't that joke so funny?! True"
+#Program will print "Isn't that joke so funny?! True"
 print(jokeEvaluation % hilarious)
 
 #Program will define w as "This is the left side of..."
@@ -41,7 +41,7 @@ print("Mary had a little lamb")
 print("Its fleece was white as %s." % 'snow')
 #Program will print "And everywhere that mary went."
 print("And everywhere that mary went.")
-#Program will print a peroid 10 time
+#Program will print a period 10 times
 print("." * 10)
 #Program will define all the end variables as the letters in the word cheeseburger
 end1 = "C"
@@ -57,5 +57,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+#Program will combine all variables (letters) together so that it says "Cheese \n Burger"
 print(end1 + end2 + end3 + end4 + end5 + end6)
 print(end7 + end8 + end9 + end10 + end11 + end12)
