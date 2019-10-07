5df859606be1ab9a1e6c897f55c586b724d221b8
lorenzoaulisa <lorenzoaulisa05@gmail.com>
2019-10-07 15:19:33 -0500

    First commit

---
+++ Learn 3.py
@@ -0,0 +1,10 @@
+# More strings and text
+
+x = "There are %d types of peoples." % 10
+binary = "binary"
+doNot = "don't"
+y = "Those who know %s and those who %s" % (binary, doNot)
+
+print(x)
+print(y)
+
